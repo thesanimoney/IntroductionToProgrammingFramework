44fd133fda31081546e607619d9d5e1dc9dfc1af
thesanimoney <72603421+thesanimoney@users.noreply.github.com>
2024-01-02 17:52:51 +0200

    Update and rename Program.cs to Program.ts

---
+++ Characters/Program.ts
@@ -0,0 +1,22 @@
+class VariableAssignment {
+    static main(): void {
+        // Assigning string values to variables
+        let universityName: string = "Software University";
+        let programmingMessage: string = "I love programming";
+
+        // Assigning character values to variables
+        let charB: string = 'B';
+        let charY: string = 'y';
+        let charE: string = 'e';
+
+        // Displaying the values of the variables
+        console.log("University Name: " + universityName);
+        console.log("Character B: " + charB);
+        console.log("Character Y: " + charY);
+        console.log("Character E: " + charE);
+        console.log("Programming Message: " + programmingMessage);
+    }
+}
+
+// Call the main method to start the program
+VariableAssignment.main();
